70e9717722fcd4a3d1402b8e4eb25f65ffad507b
diit <diit@DESKTOP-NO084R1>
2023-03-19 16:38:51 +0600

    Add project files.

---
+++ Angular_project/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Angular_project
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext()
+        { }
+
+        public MyDBContext(DbContextOptions<MyDBContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Appointment> Appointment { get; set; }
+        public DbSet<Service> Service { get; set; }
+    }
+
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+use angular
+select * from appointment where appointment_id='A-001'
+select * from service where appointment_id='A-001'
+select * from appointment
+select * from service
+
